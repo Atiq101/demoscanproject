1998ca983de0574e7063e8f965b67a8f7fd3f362
MUHAMAMD KHILJI <121760817+Atiq101@users.noreply.github.com>
2025-12-25 17:15:05 +0000

    Update provider.tf

--- environment/prod/provider.tf
+++ environment/prod/provider.tf
@@ -1,30 +1,49 @@
 terraform {
   required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.37.0"
     }
-  } 
+  }
 
-
-   backend "azurerm" {
-   
-   container_name = "container-zia-123"
-   storage_account_name = "storagezia123"
-   key = "terraform.tfstate"
-
+  backend "azurerm" {}
 }
-
-
-
-}
-
 
 provider "azurerm" {
   features {}
-  subscription_id = "621b77de-60d1-4956-b66a-3dff0ad8f059"
 }
+
+
+
+//terraform {
+ // required_version = ">= 1.6.0"
+  //required_providers {
+   // azurerm = {
+    //  source  = "hashicorp/azurerm"
+     // version = "4.37.0"
+    //}
+  //} 
+
+
+   //backend "azurerm" {
+   
+ //  container_name = "container-zia-123"
+  // storage_account_name = "storagezia123"
+   //key = "terraform.tfstate"
+
+//}
+
+
+
+//}
+
+
+//provider "azurerm" {
+ // features {}
+  //subscription_id = "621b77de-60d1-4956-b66a-3dff0ad8f059"
+//}
 
 
 
